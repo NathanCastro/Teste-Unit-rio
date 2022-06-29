ac0d46bbc76b4cfcac9db2c31b6006a73174c89a
Nathan Castro <nathanslc18@gmail.com>
2022-06-29 07:56:50 -0300

    02- lapidando nossos testes - 04- surpresa inesperada

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,21 +1,30 @@
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
+const service = new UniqueIdService();
+
+describe('UniqueIdService', () => {   
+
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-        const service = new UniqueIdService();
         const id = service.generateUniqueIdWithPrefix('app');
         expect(id.startsWith('app -')).toBeTrue();
     });
+    
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple time`, () => {
-        const service = new UniqueIdService();
         const ids = new Set();
         for (let i = 0; i < 50; i++){
             ids.add(service.generateUniqueIdWithPrefix('app'));
         }
         expect(ids.size).toBe(50);
     });  
+   
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of genetadedIds when called`, () => {
+        service.generateUniqueIdWithPrefix('app');
+        service.generateUniqueIdWithPrefix('app');
+
+        expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+    });
 })
-/*
-foi feito o for para percorrer 50 IDs, sendo que não pode ter IDs iguais
-e com isso foi usado o metodo SET que não deixa repetir.
+
+/*foi colocado o const service uma unica vez, e retirei de todos os ITs,
+esse teste deu um problema, pois os testes do Karma faz testes aleatórios e com isso ao atualizar sempre dá erro.
 */
