4c900ffdc93099f088a8fd4791142b131a185d2c
Nathan Castro <nathanslc18@gmail.com>
2022-07-04 06:46:57 -0300

    02- lapidando nossos testes - 07- tratando exceção

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -2,30 +2,45 @@ import { UniqueIdService } from './unique-id.service';
 
 
 
-describe('UniqueIdService', () => { 
-    
+describe('UniqueIdService', () => {
+
     let service: UniqueIdService = null
     beforeEach(() => {
-        const service = new UniqueIdService();
+       service = new UniqueIdService();
     })
-    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+        should generate id when called with prefix`, () => {
         const id = service.generateUniqueIdWithPrefix('app');
         expect(id.startsWith('app -')).toBeTrue();
     });
-    
-    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple time`, () => {
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should not generate duplicate IDs when called multiple time`, () => {
         const ids = new Set();
-        for (let i = 0; i < 50; i++){
+        for (let i = 0; i < 50; i++) {
             ids.add(service.generateUniqueIdWithPrefix('app'));
         }
         expect(ids.size).toBe(50);
-    });  
-   
-    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of genetadedIds when called`, () => {
+    });
+
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} 
+        should return the number of genetadedIds when called`, () => {
         service.generateUniqueIdWithPrefix('app');
         service.generateUniqueIdWithPrefix('app');
 
         expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
     });
+
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} 
+        should throw when called with empty`, () => {
+        const emptyValues = [null, undefined, '']
+        emptyValues.forEach(emptyValues =>{
+            expect(() => service.generateUniqueIdWithPrefix(emptyValues)).toThrow();
+        });
+    });
 })
 
+/*
+quando vc ta chamando um método, e vc quer testar se ele lança uma seção ou não
+vc tem q chamar esse método dentro de uma função*/
