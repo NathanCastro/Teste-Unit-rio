cf2e2ac1fe8eb950d517e8e82e49410d5a294a0f
Nathan Castro <nathanslc18@gmail.com>
2022-07-12 06:28:05 -0300

    04- Testando Component 13- Testando propriedade Output

--- src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget/like-widget.component.spec.ts
@@ -6,45 +6,48 @@ import { LikeWidgetModule } from './like-widget.module';
  
 fdescribe('LikeWidgetComponent', () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
   beforeEach(async  () => { //async na Totvs não se usa mais
     await TestBed.configureTestingModule({
-      imports: [
-        LikeWidgetModule
-      ],
+      imports: [LikeWidgetModule],
     }).compileComponents();
     
     fixture = TestBed.createComponent(LikeWidgetComponent)
+    component = fixture.componentInstance;
   })
 
   it('should create component', () => {
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
-  it('should auto generate ID when id input property is missing', () => {
-    const component = fixture.componentInstance;
+  it('should auto generate ID when id input property is missing', () => {    
     fixture.detectChanges();
     expect(component.id).toBeTruthy();
   });
-  it('should NOT generate ID when id input propertu is present', () => {
-    const component = fixture.componentInstance;
+  it('should NOT generate ID when id input propertu is present', () => {    
     const someId = 'someId';
     component.id= someId
     fixture.detectChanges();
     expect(component.id).toBe(someId);
   });
 
+  it(`#${LikeWidgetComponent.prototype.likeWidgetComponentClik.name}
+    should trigger emission when called`, () => {    
+    fixture.detectChanges();
+    component.likedWidgetComponent.subscribe(()=>{
+      expect(true).toBeTrue();
+    });
+    component.likeWidgetComponentClik()
+  });
+
+
+  `A respeito ao teste de propriedades de output:
+  
+  É possível realizar a inscrição de propriedades de 
+  output em nossos testes como se fossem Observables.` 
+
+
 
 });
 
-`a respeito da detecção de mudanças de um componente ao executarmos nossos testes:
-  É possível tornar a detecção de mudanças automática em 
-  nossos testes, apesar da equipe do Angular não recomendá-la.
-
-  Por padrão, a detecção de mudanças de um componente não é disparada automaticamente ao executarmos
-  nossos testes. É preciso que o desenvolvedor faça essa detecção manualmente.
-
-  É através de uma fixture de um component que disparamos o mecanismo de detecção de mudanças do Angular
-  e por conseguinte o seu ciclo de vida.
-`
